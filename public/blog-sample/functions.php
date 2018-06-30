dc4700c0a5939f4fe447ce904ed81c6375eb05a2
YutaOikawa <oikenfight@gmail.com>
2018-06-30 10:07:23 +0900

    fix it

--- public/blog-sample/functions.php
+++ public/blog-sample/functions.php
@@ -4,11 +4,10 @@
 function connectDB()
 {
     $config = require_once('./config.php');
-    $dsn = "mysql:dbname=" . $config['db']['database'] . ";host=" . $config['db']['host'];
+    $dsn = "mysql:dbname=" . $config['db']['database'] . ";host=" . $config['db']['host'].';charset=utf8;';
 
     try {
         $db = new PDO($dsn, $config['db']['user'], $config['db']['password']);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch (PDOException $e) {
         die('Connecting database failed:' . $e->getMessage());
     }
